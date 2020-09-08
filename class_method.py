fa7bfe20918fd76bfebd0fc52f1d5249496522d9
archit.agarwal <archit.agarwal@afourtech.com>
2020-09-08 11:51:41 +0530

    hardcoded i/p

--- class_method.py
+++ class_method.py
@@ -8,10 +8,10 @@ class Math:
 		self.result=z
 		return self.result
 a=Math()
-num1 = input("Enter teh first number:")
-num2 = input("Enter teh second number")
-num1 = int(num1)
-num2 = int(num2)
+#num1 = input("Enter teh first number:")
+#num2 = input("Enter teh second number")
+num1 = 10
+num2 = 7
 d=(a.add(num1, num2))
 e=(a.sub(num1, num2))
 print('{0},{1}'.format(d,e))
